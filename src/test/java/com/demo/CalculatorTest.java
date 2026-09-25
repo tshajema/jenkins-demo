a5aa42f99a6d2a89ed8f06a8d075965cf164bddf
Deiza <deiza@Deizas-MacBook-Pro.local>
2026-09-25 14:03:23 +0300

    initial commit

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        Calculator calculator = new Calculator();
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+}
